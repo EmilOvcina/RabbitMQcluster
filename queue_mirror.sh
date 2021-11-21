52d8721aeccddb763570d4765441a607207058a3
= <=>
2021-11-21 14:38:24 +0100

    latest

--- queue_mirror.sh
+++ queue_mirror.sh
@@ -3,9 +3,9 @@
 kubectl -n rabbits exec -it rabbitmq-0 rabbitmqctl set_policy ha-fed '.*' "{'federation-upstream-set':'all', 'ha-sync-mode':'automatic', 'ha-mode':'nodes', 'ha-params':['rabbit@rabbitmq-0.rabbitmq.rabbits.svc.cluster.local','rabbit@rabbitmq-1.rabbitmq.rabbits.svc.cluster.local']}" --priority 1 --apply-to queues
 # rabbitmqctl set_policy ha-fed ".*" '{"federation-upstream-set":"all", "ha-sync-mode":"automatic", "ha-mode":"nodes", "ha-params":["rabbit@rabbitmq-0.rabbitmq.rabbits.svc.cluster.local","rabbit@rabbitmq-1.rabbitmq.rabbits.svc.cluster.local"]}' --priority 1 --apply-to queues
 
-# rabbitmqctl set_policy ha-fed \
-#     ".*" '{"federation-upstream-set":"all", "ha-sync-mode":"automatic", "ha-mode":"nodes", "ha-params":["rabbit@rabbitmq-0.rabbitmq.rabbits.svc.cluster.local","rabbit@rabbitmq-1.rabbitmq.rabbits.svc.cluster.local"]}' \
-#     --priority 1 \
-#     --apply-to queues
+rabbitmqctl set_policy ha-fed \
+    ".*" '{"federation-upstream-set":"all", "ha-sync-mode":"automatic", "ha-mode":"nodes", "ha-params":["rabbit@rabbitmq-0.rabbitmq.rabbits.svc.cluster.local","rabbit@rabbitmq-1.rabbitmq.rabbits.svc.cluster.local"]}' \
+    --priority 1 \
+    --apply-to queues
 
 # exit 
